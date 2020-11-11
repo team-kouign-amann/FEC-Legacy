d80cc6f44d982271045d85a6440e60aeb1c665b4
Nate Pruitt <nlpruitt@gmail.com>
2020-11-11 11:39:19 -0500

    Created linter to follow airbnb style guide. Adjusted files accordingly.

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
-const express = require("express");
-const path = require("path");
+const express = require('express');
+
+const path = require('path');
+
 const app = express();
+
 const PORT = 3000;
 
 app.use(express.static(path.join(__dirname, 'dist')));
 
-app.listen(PORT, console.log("listening on port: ", PORT, "."));
+app.listen(PORT, () => { console.log('listening on port: ', PORT, '.'); });
