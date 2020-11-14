8bcd7ebc596d4ef166af36193737d4a38aaa1ab6
Matthew Schneiderman <matthewdschneiderman@gmail.com>
2020-11-14 14:01:28 -0500

    Added tests

--- client/components/App.test.js
+++ client/components/App.test.js
@@ -1,10 +1,39 @@
-import React from 'react'
-import {shallow} from 'enzyme';
-
+import React from 'react';
+import { shallow } from 'enzyme';
 import App from './App.jsx';
+import Product from '../containers/ProductContainer.js';
+import Questions from '../containers/QuestionsContainer.js';
+import Ratings from '../containers/RatingsContainer.js';
+import Related from '../containers/RelatedContainer.js';
 
-test('App contains `Hello World!`', () => {
-  const wrapper = shallow(<App />);
-  console.log(wrapper.debug());
-  expect(wrapper.contains('Hello World!')).toBe(true);
-})
+describe('App', () => {
+  let appWrapper;
+
+  beforeAll(() => {
+    appWrapper = shallow(<App />);
+  });
+
+  test('App contains `Hello World!`', () => {
+    console.log(appWrapper.debug());
+    expect(appWrapper.contains('Hello World!')).toBe(true);
+  });
+
+  test('renders Product Container', () => {
+    const questions = appWrapper.find(Product);
+    expect(questions).toHaveLength(1);
+  });
+
+  test('renders Questions Container', () => {
+    const questions = appWrapper.find(Ratings);
+    expect(questions).toHaveLength(1);
+  });
+
+  test('renders Ratings Container', () => {
+    const questions = appWrapper.find(Related);
+    expect(questions).toHaveLength(1);
+  });
+  test('renders Related Container', () => {
+    const questions = appWrapper.find(Questions);
+    expect(questions).toHaveLength(1);
+  });
+});
