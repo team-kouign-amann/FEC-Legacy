037e6496d38cc1f5295352129ef15650c92846d6
Nate Pruitt <nlpruitt@gmail.com>
2020-11-15 14:38:47 -0500

    Adjusted code to follow style guide

--- client/components/widgets/Product/Product.jsx
+++ client/components/widgets/Product/Product.jsx
@@ -1,12 +1,23 @@
 import React from "react";
 
-const Product = () => {
-  return(
+const Product = () => (
+  <div>
     <div><h1>Product</h1></div>
-  )
-}
-
-
-
+    <div className="container">
+      <div className="grid-container">
+        <div className="item1">
+          Carousel
+          <img src="http://placecorgi.com/250" alt="" />
+        </div>
+        <div className="item2">Category</div>
+        <div className="item3">Style</div>
+        <div className="item4">Bag</div>
+        <div className="item5">Size</div>
+        <div className="item6">Slogan</div>
+        <div className="item7">Guarantee</div>
+      </div>
+    </div>
+  </div>
+);
 
 export default Product;
