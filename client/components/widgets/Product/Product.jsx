9feb87c159abe2e61827ffbb3ad2e7b788db158a
Nate Pruitt <nlpruitt@gmail.com>
2020-11-15 22:08:30 -0500

    Change HTML to include carousel buttons and image

--- client/components/widgets/Product/Product.jsx
+++ client/components/widgets/Product/Product.jsx
@@ -6,8 +6,16 @@ const Product = () => (
     <div className="container">
       <div className="grid-container">
         <div className="item1">
-          Carousel
-          <img src="http://placecorgi.com/250" alt="" />
+          <button className="up-button" type="submit"> up </button>
+          <ul className="preview_list">
+            <li><img src="http://placecorgi.com/100" /></li>
+            <li><img src="http://placecorgi.com/100" /></li>
+            <li><img src="http://placecorgi.com/100" /></li>
+          </ul>
+          <button className="down-button" type="submit"> down </button>
+          <button className="left-button" type="submit"> Left </button>
+          <img className="center-image" src="http://placecorgi.com/450" alt="" />
+          <button className="right-button" type="submit"> Right </button>
         </div>
         <div className="item2">Category</div>
         <div className="item3">Style</div>
