d305667bbc8d873d3a30cb9472ad406391b92d8d
Nate Pruitt <nlpruitt@gmail.com>
2020-11-16 22:59:33 -0500

    Added store.dispatch to index.jsx (fetching initial data)

--- client/index.jsx
+++ client/index.jsx
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import App from './components/App.jsx';
 import store from './store/store.js';
+import getProduct from './actions/productOverview/getProduct.js'
+import getStyles from './actions/productOverview/getStyles.js';
+
+
+store.dispatch(getProduct(2));
+store.dispatch(getStyles(3));
 
 ReactDOM.render(
   <Provider store={store}>
