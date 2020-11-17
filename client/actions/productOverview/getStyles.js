0759f57814677335dd8cee027bb2c487420770ff
Nate Pruitt <nlpruitt@gmail.com>
2020-11-16 23:00:24 -0500

    Implemented  action creators for 'productOverview' reducers

---
+++ client/actions/productOverview/getStyles.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+import currentStyleAction from './currentStyle.js';
+import allStylesAction from './allStyles.js';
+
+
+
+const getStyles = (productID) => {
+  return (dispatch) => {
+    return axios.get('http://3.21.164.220/products/' + productID + '/styles')
+    .then(({data}) => {
+      console.log(data.results);
+      console.log(data.results[0]);
+      dispatch(allStylesAction(data.results));
+      dispatch(currentStyleAction(data.results[0]));
+    })
+    .catch((err) => {
+      console.log("Error fetching style! Error: ", err);
+    })
+  }
+}
+
+export default getStyles;
