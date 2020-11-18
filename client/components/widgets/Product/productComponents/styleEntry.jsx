270a161ebc2d4ad0f779ed53c22d50f31410231c
Nate Pruitt <nlpruitt@gmail.com>
2020-11-18 12:09:07 -0500

    Added 'onClick' handler to style entry component

--- client/components/widgets/Product/productComponents/styleEntry.jsx
+++ client/components/widgets/Product/productComponents/styleEntry.jsx
@@ -1,24 +1,23 @@
 import React from 'react';
 import colorArray from '../../../../../util/colorArray.js';
 
-const StyleEntry = ({style}) => {
-  const colors = colorArray(style.name);
-  console.log(colors);
 
+const StyleEntry = ({style, handleClick}) => {
+  const colors = colorArray(style.name);
   if (colors.length === 0) {
     return (
-      <li className="style-select-img horizontal-list" style={{'background':'grey'}}>
-
+      <li className="style-select-img horizontal-list" style={{'background':'grey'}} >
+        <button type="submit" onClick={() => { handleClick(style) }} />
       </li>
     )
   } else if (colors.length === 1) {
     return (
-      <li className="style-select-img horizontal-list" style = {{'background': `${colors[0]}`}} >
+      <li className="style-select-img horizontal-list" style = {{'background': `${colors[0]}`}}>
       </li>
     )
   } else if (colors.length === 2) {
     return (
-      <li className="style-select-img horizontal-list" style={{'background':`linear-gradient(-45deg, ${colors[0]}, ${colors[0]} 49%, white 49%, white 51%, ${colors[1]} 51%`}}>
+      <li className="style-select-img horizontal-list" style={{'background':`linear-gradient(-45deg, ${colors[0]}, ${colors[0]} 49%, white 49%, white 51%, ${colors[1]} 51%`}} onClick={() => { handleClick(style) }} >
       </li>
     )
   } else {
