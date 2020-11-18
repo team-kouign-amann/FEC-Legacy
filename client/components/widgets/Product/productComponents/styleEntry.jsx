cc36d81d9e701f7608bf62e3126f61ba8bbc8eee
Nate Pruitt <nlpruitt@gmail.com>
2020-11-17 23:48:50 -0500

    Finalized 'styleEntry' to style with colors

--- client/components/widgets/Product/productComponents/styleEntry.jsx
+++ client/components/widgets/Product/productComponents/styleEntry.jsx
@@ -1,9 +1,34 @@
 import React from 'react';
+import colorArray from '../../../../../util/colorArray.js';
 
-const StyleEntry = ({style}) => (
-  <li className="style-select-img horizontal-list" style={{'background':'linear-gradient(-45deg, indigo, indigo 49%, white 49%, white 51%, red 51%'}}>
-  </li>
+const StyleEntry = ({style}) => {
+  const colors = colorArray(style.name);
+  console.log(colors);
 
-);
+  if (colors.length === 0) {
+    return (
+      <li className="style-select-img horizontal-list" style={{'background':'grey'}}>
+
+      </li>
+    )
+  } else if (colors.length === 1) {
+    return (
+      <li className="style-select-img horizontal-list" style = {{'background': `${colors[0]}`}} >
+      </li>
+    )
+  } else if (colors.length === 2) {
+    return (
+      <li className="style-select-img horizontal-list" style={{'background':`linear-gradient(-45deg, ${colors[0]}, ${colors[0]} 49%, white 49%, white 51%, ${colors[1]} 51%`}}>
+      </li>
+    )
+  } else {
+    return (
+      <li className="style-select-img horizontal-list" style={{'background':'grey'}}>
+
+      </li>
+    )
+  }
+
+};
 
 export default StyleEntry;
