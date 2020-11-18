1fd2af52df160f3c87acdf65a36bc37412f20f1e
Nate Pruitt <nlpruitt@gmail.com>
2020-11-18 12:09:37 -0500

    Added redux container to style selector component

--- client/components/widgets/Product/productComponents/styleSelector.jsx
+++ client/components/widgets/Product/productComponents/styleSelector.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import StyleEntry from './styleEntry.jsx';
+import StyleEntryContainer from '../../../../containers/productContainers/styleEntryContainer.js';
 
 const StyleSelector = ({styles}) => (
   <div className="item3">
-    <span className="selected-font"><b>Style ></b> Selected Style... </span>
+    <span className="selected-font"><b>Style </b> Selected Style... </span>
     <ul className="remove-bullet">
       {styles.map((style) => (
-        <StyleEntry style={style} />
+        <StyleEntryContainer style={style} />
       ))}
     </ul>
   </div>
-)
+);
 
 export default StyleSelector;
