aef25705b88703ff4515deac0caf89e2cb692d20
Nate Pruitt <nlpruitt@gmail.com>
2020-11-18 19:19:28 -0500

    Added carousel container to product container

--- client/components/widgets/Product/Product.jsx
+++ client/components/widgets/Product/Product.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import Carousel from "./productComponents/Carousel.jsx";
 import StyleSelector from "./productComponents/styleSelector.jsx";
 import StyleSelectorContainer from '../../../containers/productContainers/styleSelectorContainer.js';
+import CarouselContainer from '../../../containers/productContainers/carouselContainer.js';
 
 const Product = ({product, currentStyle, styles}) => (
   <div>
     <div><h1>Product</h1></div>
     <div className="container">
       <div className="grid-container">
-        <Carousel currentStyle={currentStyle}/>
+        <CarouselContainer />
         <div className="item2">
           <span className="category-name-font">{product.category}</span>
           <span className="product-name-font">{product.name}</span>
