4483ef0562570e93d94f547c5bb1b3f7c85794d4
Nate P <nlpruitt@gmail.com>
2020-11-19 21:26:41 -0500

    Implemented 'onClick' for preview list entry

--- client/components/widgets/Product/productComponents/PreviewListEntry.jsx
+++ client/components/widgets/Product/productComponents/PreviewListEntry.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
-const PreviewListEntry = ({currentStyle, index}) => (
-  <li><img src={currentStyle.photos[index].thumbnail_url} className="thumbnail-img" alt="" /></li>
+const PreviewListEntry = ({currentStyle, index, handlePreviewClick}) => (
+  <li onClick={()=> {handlePreviewClick(index)}}><img src={currentStyle.photos[index].thumbnail_url} className="thumbnail-img" alt="" /></li>
 );
 
 export default PreviewListEntry;
