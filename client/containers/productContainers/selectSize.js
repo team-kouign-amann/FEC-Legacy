c4884b55423c653c815c428857dbfcbe1b753bcb
Nate P <nlpruitt@gmail.com>
2020-11-20 23:22:25 -0500

    Begin tracking 'maxQuantity' in store

--- client/containers/productContainers/selectSize.js
+++ client/containers/productContainers/selectSize.js
@@ -1,6 +1,6 @@
 import { connect } from 'react-redux';
 import SelectSize from '../../components/widgets/Product/productComponents/SelectSize.jsx';
-import toggleSelectSizeAction from '../../actions/productOverview/selectSize.js';
+import { toggleSelectSizeAction, selectSizeAction, updateMaxQuantityAction } from '../../actions/productOverview/selectSize.js';
 
 const mapStateToProps = (state) => ({
   currentStyle: state.currentStyle,
@@ -9,6 +9,8 @@ const mapStateToProps = (state) => ({
 
 const mapDispatchToProps = (dispatch) => ({
   handleSelectClick: () => { dispatch(toggleSelectSizeAction(null)); },
+  handleSelectChange: (size) => { dispatch(selectSizeAction(size)); },
+  updateMaxQuantity: (quantity) => {dispatch(updateMaxQuantityAction(quantity)); },
 });
 
 const SelectSizeContainer = connect(mapStateToProps, mapDispatchToProps)(SelectSize);
--- client/reducers/productOverview/selectSize.js
+++ client/reducers/productOverview/selectSize.js
@@ -1,13 +1,23 @@
-const selectSizeReducer = (state = true, action) => {
+const initialState = {
+  currentSize: 'SELECT SIZE',
+  display: false,
+  maxQuantity: 0,
+};
+
+const selectSizeReducer = (state = initialState, action) => {
   if (action.type === 'SELECT-SIZE') {
-      if (state === true) {
-        return false;
+      if (state.display === true) {
+        return {...state, display: false};
       } else {
-        return true;
+        return {...state, display: true};
     }
+  } else if (action.type === 'CURRENT-SIZE') {
+      return {...state, currentSize: action.payload};
+  } else if (action.type === 'MAX-QUANTITY') {
+    return {...state, maxQuantity: action.payload};
   } else {
-      return state;
-    }
+    return state;
+  }
 };
 
 export default selectSizeReducer;
