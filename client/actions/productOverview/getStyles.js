4be8522aacbcf1742515b87a25458e313ef7a3e2
Nate Pruitt <nlpruitt@gmail.com>
2020-11-24 17:16:57 -0500

    Clean up getStyles

--- client/actions/productOverview/getStyles.js
+++ client/actions/productOverview/getStyles.js
@@ -1,18 +1,17 @@
 import axios from 'axios';
 import currentStyleAction from './currentStyle.js';
 import allStylesAction from './allStyles.js';
-import {previewListFirstAction, previewListLastAction} from './previewList.js';
+
 
 
 const getStyles = (productID) => {
   return (dispatch) => {
     return axios.get('http://3.21.164.220/products/' + productID + '/styles')
     .then(({data}) => {
-      //console.log(data.results);
-      //console.log(data.results[0]);
+      // console.log(data.results);
+      // console.log(data.results[0]);
       dispatch(allStylesAction(data.results));
       dispatch(currentStyleAction(data.results[0]));
-      dispatch(previewListLastAction(data.results[0]['photos'].length - 1));
     })
     .catch((err) => {
       console.log("Error fetching style! Error: ", err);
