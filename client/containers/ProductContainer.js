08400c12ec4c57b077d1d64a0ce0b5e1e7274d30
Nate Pruitt <nlpruitt@gmail.com>
2020-11-25 16:29:30 -0500

    add expanded view to Product props

--- client/containers/ProductContainer.js
+++ client/containers/ProductContainer.js
@@ -1,13 +1,19 @@
 import { connect } from 'react-redux';
 import Product from '../components/widgets/Product/Product.jsx';
+import expandedViewAction from '../actions/productOverview/expandedView.js';
 
-
-var mapStateToProps = (state) => ({
+const mapStateToProps = (state) => ({
   product: state.currentProduct,
   currentStyle: state.currentStyle,
-  styles: state.styles
+  styles: state.styles,
+  carouselPosition: state.carouselPosition,
+  expandedView: state.expandedView,
 });
 
-var ProductContainer = connect(mapStateToProps, null)(Product);
+const mapDispatchToProps = (dispatch) => ({
+  handleImageClick: () => { dispatch(expandedViewAction()); },
+});
+
+const ProductContainer = connect(mapStateToProps, mapDispatchToProps)(Product);
 
 export default ProductContainer;
