193947d3b6473c3276bf7ecd084912124c932c27
Nate Pruitt <nlpruitt@gmail.com>
2020-11-25 21:00:15 -0500

    Add handler for Zoom view and expanded view

--- client/containers/productContainers/expandedViewContainer.js
+++ client/containers/productContainers/expandedViewContainer.js
@@ -1,16 +1,18 @@
 import { connect } from 'react-redux';
 import ExpandedView from '../../components/widgets/Product/productComponents/ExpandedView.jsx';
 import carouselPositionAction from '../../actions/productOverview/carouselPosition.js';
-import expandedViewAction from '../../actions/productOverview/expandedView.js';
+import { expandedViewAction, zoomViewAction } from '../../actions/productOverview/expandedView.js';
 
 const mapStateToProps = (state) => ({
   currentStyle: state.currentStyle,
   carouselPosition: state.carouselPosition,
+  toggleZoom: state.expandedView.toggleZoom,
 });
 
 const mapDispatchToProps = (dispatch) => ({
   handleCarouselChange: (position) => { dispatch(carouselPositionAction(position)); },
-  handleImageClick: () => { dispatch(expandedViewAction()); },
+  handleImageClick: () => { dispatch(zoomViewAction()); },
+  exitExpandedView: () => { dispatch(expandedViewAction()); },
 });
 
 const ExpandedViewContainer = connect(mapStateToProps, mapDispatchToProps)(ExpandedView);
