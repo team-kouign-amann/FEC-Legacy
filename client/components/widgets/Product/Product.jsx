1eeacea5f012bdcf51f52a9c1074d48c349153c5
Nate Pruitt <nlpruitt@gmail.com>
2020-11-26 10:34:15 -0500

    Convert product to class based component

--- client/components/widgets/Product/Product.jsx
+++ client/components/widgets/Product/Product.jsx
@@ -6,32 +6,36 @@ import ProductInfoContainer from '../../../containers/productContainers/productI
 import SloganContainer from '../../../containers/productContainers/sloganContainer.js';
 import ExpandedViewContainer from '../../../containers/productContainers/expandedViewContainer.js';
 
-const Product = ({currentStyle, carouselPosition, expandedView, handleImageClick}) => (
-  <div>
-  {expandedView ?
-   <ExpandedViewContainer /> : null}
-  <div style={{"margin":"0px 150px"}}>
-    <div className="header-container">
-      <div className="header-logo">
-        Daifuku X Donqueello Collab Official Store
+class Product extends React.Component {
+  render() {
+    return (
+      <div>
+        {this.props.expandedView ?
+          <ExpandedViewContainer /> : null}
+        <div style={{"margin":"0px 150px"}}>
+          <div className="header-container">
+            <div className="header-logo">
+              Daifuku X Donqueello Collab Official Store
+            </div>
+            <div className="header-search-container">
+              <input className="header-search-bar" placeholder="____________" />
+            </div>
+          </div>
+          <div className="container">
+            <div className="grid-container">
+              <CarouselContainer />
+              <ProductInfoContainer />
+              <StyleSelectorContainer />
+              <SelectSizeContainer />
+              <div className="item5">Size</div>
+              <SloganContainer />
+              <div className="item7">Guarantee</div>
+            </div>
+          </div>
+        </div>
       </div>
-      <div className="header-search-container">
-        <input className="header-search-bar" placeholder="____________" />
-      </div>
-    </div>
-    <div className="container">
-      <div className="grid-container">
-        <CarouselContainer />
-        <ProductInfoContainer />
-        <StyleSelectorContainer />
-        <SelectSizeContainer />
-        <div className="item5">Size</div>
-        <SloganContainer />
-        <div className="item7">Guarantee</div>
-      </div>
-    </div>
-  </div>
-  </div>
-);
+    );
+  }
+};
 
 export default Product;
