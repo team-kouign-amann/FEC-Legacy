d1424ac10f526fe071f6541df925b613a67bb294
Nate Pruitt <nlpruitt@gmail.com>
2020-11-25 20:57:21 -0500

    Add left/right buttons to expanded view

--- client/components/widgets/Product/productComponents/ExpandedView.jsx
+++ client/components/widgets/Product/productComponents/ExpandedView.jsx
@@ -1,13 +1,30 @@
 import React from 'react';
 
-const ExpandedView = ({currentStyle, carouselPosition, handleImageClick}) => (
-  <React.Fragment key="key">
-    <div class={"overlay"}></div>
-    <div class={"overlayContent"}>
-      <img src={currentStyle.photos[carouselPosition].url !== null ? currentStyle.photos[carouselPosition].url : ''} alt=''
-      onClick={()=> {handleImageClick()}} />
-    </div>
-  </React.Fragment>
-);
+const ExpandedView = ({currentStyle, carouselPosition, handleImageClick, handleCarouselChange, toggleZoom, exitExpandedView}) => {
+  let leftButtonClass = 'arrow left';
+  let rightButtonClass = 'arrow right';
+  if (carouselPosition === 0) {
+    leftButtonClass += ' hidden';
+  }
+  if (carouselPosition === currentStyle.photos.length - 1) {
+    rightButtonClass += ' hidden';
+  }
+  const style = {"transform": "scale(2.5)"};
+  return (
+    <React.Fragment key="key">
+      <div class={"overlay"} onClick={() => {exitExpandedView()}} ></div>
+      <div class={"overlayContent"}>
+        <div onClick={()=> {handleImageClick(toggleStyle)}}></div>
+        <div className={leftButtonClass} style={{width: '25px', height: '25px'}}
+        onClick={() => { handleCarouselChange(carouselPosition - 1); }}
+            onMouseDown={(e) => {e.preventDefault()}}></div>
+        <img src={currentStyle.photos[carouselPosition].url !== null ? currentStyle.photos[carouselPosition].url : ''} alt='' style={toggleZoom ? style : {}} onClick={()=> {handleImageClick()}} />
+        <div className={rightButtonClass} style={{width: '25px', height: '25px'}} onClick={() => { handleCarouselChange(carouselPosition + 1); }}
+      onMouseDown={(e) => {e.preventDefault()}}></div>
+        <div ></div>
+      </div>
+    </React.Fragment>
+  );
+};
 
 export default ExpandedView;
