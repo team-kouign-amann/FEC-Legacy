ce13c0026173d23bd734f95dbaf8c11a86c95265
Nate Pruitt <nlpruitt@gmail.com>
2020-11-26 13:41:24 -0500

    Add routing to app

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,22 +1,26 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
 import RelatedContainer from '../containers/RelatedContainer.js';
 import QuestionsContainer from '../containers/QuestionsContainer.js';
 import RatingsContainer from '../containers/RatingsContainer.js';
 import ProductContainer from '../containers/ProductContainer.js';
 
 const App = () => (
-  <div>
+  <Router>
+    <div>
+      <Route path={"/:productId"}>
 
-      <ProductContainer />
+        <ProductContainer />
 
-      <RelatedContainer />
+        <RelatedContainer />
 
+        <QuestionsContainer />
 
-      <QuestionsContainer />
-
-      <RatingsContainer />
-  </div>
-
+        <RatingsContainer />
+      </Route>
+    </div>
+  </Router>
 
 );
 
