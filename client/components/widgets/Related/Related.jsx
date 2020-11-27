0f71745f48fb0caf5cde85de798ea729c243da40
Christopher <Christopherliang7@gmail.com>
2020-11-27 17:43:33 -0500

    etge

--- client/components/widgets/Related/Related.jsx
+++ client/components/widgets/Related/Related.jsx
@@ -1,30 +1,36 @@
-import React from "react";
+/* eslint-disable import/no-named-as-default */
+/* eslint-disable import/no-named-as-default-member */
+/* eslint-disable react/prop-types */
+/* eslint-disable react/destructuring-assignment */
+import React from 'react';
 import ProductCarousel from './Carousels/ProductCarousel.jsx';
 import OutfitCarousel from './Carousels/OutfitCarousel.jsx';
-import ComparisonModal from './ComparisonModal.jsx'
 
 class Related extends React.Component {
   constructor(props) {
-    super(props)
-    this.state = {
-      compare: false
-    }
+    super(props);
+    this.state = {};
   }
 
   componentDidMount() {
-    this.props.getRelatedProducts();
+    console.log(this.props);
+    const overviewProduct = this.props.overviewProduct.id;
+    this.props.getRelatedProducts(overviewProduct);
+    // this.props.getOutfitInformation(overviewProduct);
+    // testing
+    this.props.getOutfitInformation(77);
   }
 
   render() {
     return (
       <div>
-        {/* { this.state.compare ? <ComparisonModal /> : <></> } */}
-
-        <ProductCarousel relatedInfo={this.props.relatedInfo} />
-
-        <OutfitCarousel />
+        <ProductCarousel
+          relatedInfo={this.props.relatedInfo}
+          overviewProduct={this.props.overviewProduct}
+        />
+        <OutfitCarousel outfitInfo={this.props.outfitInfo} />
       </div>
-    )
+    );
   }
 }
 
