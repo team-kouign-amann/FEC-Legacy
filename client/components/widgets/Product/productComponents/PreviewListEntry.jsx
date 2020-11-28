08795c2e8488811ef7917f00da83f49f2e46fac2
Nate Pruitt <nlpruitt@gmail.com>
2020-11-27 21:06:55 -0500

    Adjust preview list entry to handle undefined or null photos

--- client/components/widgets/Product/productComponents/PreviewListEntry.jsx
+++ client/components/widgets/Product/productComponents/PreviewListEntry.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import defaultImg from '../../../../../image-not-available.jpg';
 
 const PreviewListEntry = ({currentStyle, index, handlePreviewClick, carouselPosition}) => (
-  <li onClick={()=> {handlePreviewClick(index)}}><img src={currentStyle.photos[index].thumbnail_url !== null ? currentStyle.photos[index].thumbnail_url : defaultImg}
+  <li onClick={()=> {handlePreviewClick(index)}}><img src={currentStyle.photos !== undefined && currentStyle.photos[index].thumbnail_url !== null ? currentStyle.photos[index].thumbnail_url : defaultImg}
   className={index === carouselPosition ? "thumbnail-img thumbnail-selected" : "thumbnail-img"} alt="" /></li>
 );
 
