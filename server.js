632f4717c738ad2ca800dc82106d6a4a110238f8
Nate Pruitt <nlpruitt@gmail.com>
2020-11-29 14:21:49 -0500

    Server handles new routing

--- server.js
+++ server.js
@@ -8,12 +8,14 @@ const PORT = 3000;
 
 app.use(express.static(path.join(__dirname, 'dist')));
 
-app.get('/*', (req, res) => {
+app.get('*', (req, res) => {
+  console.log(path.join(__dirname, 'dist'));
   res.sendFile(path.join(__dirname, 'dist/index.html'), (err) => {
     if (err) {
       res.status(500).send(err);
     }
   });
 });
+
 
 app.listen(PORT, () => { console.log('listening on port: ', PORT, '.'); });
